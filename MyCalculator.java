262530ad3fa55e9b04ea6fe7d3b6c1366f88a07e
Prasanth3121 <109007745+Prasanth3121@users.noreply.github.com>
2022-07-11 16:46:33 +0530

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class MyCalculator {
+public static void main(String[] args) {
+	Calculator obj=new Calculator();
+	obj.add(12, 84, 24);
+	obj.sub(424, 240);
+	obj.mul(524, 12);
+	obj.div(145, 2);
+}
+}
