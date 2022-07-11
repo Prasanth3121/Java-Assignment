6642b20662ab2365f84f28cb75d4b2ee00494666
Prasanth3121 <109007745+Prasanth3121@users.noreply.github.com>
2022-07-11 16:32:50 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class Chrome {
+	public void getName() {
+		System.out.println("This is Google Chrome");
+	}
+
+	public static void main(String[] args) {
+
+		Chrome obj = new Chrome();
+		obj.getName();
+	}
+
+}
