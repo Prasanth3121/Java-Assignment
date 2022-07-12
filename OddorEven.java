d3ecfeadfcd856e78e44f15d1b2e3922ff17a2f5
Prasanth3121 <109007745+Prasanth3121@users.noreply.github.com>
2022-07-12 11:39:38 +0530

    Add files via upload

---
+++ OddorEven.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class OddorEven {
+	static int value = 1424;
+public static void main(String[] args) {
+	int mod=value%2;
+	if (mod==0) {
+System.out.println("The Number is Even");		
+	}else {
+		System.out.println("The Number is Odd");
+		
+	}
+}
+
+}
