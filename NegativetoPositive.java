3aeab42a350e6c597adad1d78a5be6bdedf3438a
Prasanth3121 <109007745+Prasanth3121@users.noreply.github.com>
2022-07-12 12:40:05 +0530

    Add files via upload

---
+++ NegativetoPositive.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class NegativetoPositive {	
+public static void main(String[] args) {
+int number=-40;
+if (number<0) {
+	System.out.println("The number is Negative");
+}
+int number2=number*-1;
+System.out.println("The converted positive number is:" +number2);
+}
+}
